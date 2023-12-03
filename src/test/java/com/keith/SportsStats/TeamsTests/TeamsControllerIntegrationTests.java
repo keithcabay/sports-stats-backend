fce89fb4e60a5e389d74c84d1c1575e9eaae7319
Keith Cabay <keith.cabay10@gmail.com>
2023-12-03 03:50:31 -0500

    Created integration tests for TeamsController.java

--- src/test/java/com/keith/SportsStats/TeamsTests/TeamsControllerIntegrationTests.java
+++ src/test/java/com/keith/SportsStats/TeamsTests/TeamsControllerIntegrationTests.java
@@ -16,6 +16,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static jdk.dynalink.linker.support.Guards.isNull;
+
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -63,4 +65,137 @@ public class TeamsControllerIntegrationTests {
                 MockMvcResultMatchers.jsonPath("$.fullName").value("Milwaukee Bucks")
         );
     }
+
+    @Test
+    public void testThatGetAllTeamsSuccessfullyReturnHttp200Successful() throws Exception{
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/teams")
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        );
+    }
+
+    @Test
+    public void testThatGetAllTeamsReturnsCorrectListOfTeams() throws Exception {
+        TeamsEntity team1 = TestData.createKnicksTeam();
+        TeamsEntity team2 = TestData.createBucksTeam();
+
+        TeamsEntity returnedTeam1 = teamsService.save(team1);
+        TeamsEntity returnedTeam2 = teamsService.save(team2);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/teams")
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("[0].shortName").value(returnedTeam1.getShortName())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("[0].fullName").value(returnedTeam1.getFullName())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("[1].shortName").value(returnedTeam2.getShortName())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("[1].fullName").value(returnedTeam2.getFullName())
+        );
+    }
+
+    @Test
+    public void testThatGetTeamByNameReturnsHttp200SuccessfulIfFound() throws Exception {
+        TeamsEntity team1 = TestData.createKnicksTeam();
+        TeamsEntity returnedTeam1 = teamsService.save(team1);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/teams/" + team1.getShortName())
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        );
+    }
+
+    @Test
+    public void testThatGetTeamByNameReturnsHttp200SuccessfulIfNotFound() throws Exception {
+        TeamsEntity team1 = TestData.createKnicksTeam();
+        TeamsEntity returnedTeam1 = teamsService.save(team1);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/teams/" + 9999)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNotFound()
+        );
+    }
+
+    @Test
+    public void testThatGetTeamByNameReturnsCorrectTeam() throws Exception {
+        TeamsEntity team1 = TestData.createKnicksTeam();
+        TeamsEntity returnedTeam1 = teamsService.save(team1);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/teams/" + returnedTeam1.getShortName())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.shortName").value(returnedTeam1.getShortName())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.fullName").value(returnedTeam1.getFullName())
+        );
+    }
+
+    @Test
+    public void testThatFullUpdateTeamReturnsHttp200Successful() throws Exception{
+        TeamsEntity team1 = TestData.createKnicksTeam();
+        TeamsEntity returnedTeam1 = teamsService.save(team1);
+
+        team1.setFullName("UPDATED FULL NAME");
+        String json = objectMapper.writeValueAsString(team1);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put("/teams/" + returnedTeam1.getShortName())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        );
+    }
+
+    @Test
+    public void testThatFullUpdateTeamReturnsUpdatedTeamKeepingShortName() throws Exception{
+        TeamsEntity team1 = TestData.createKnicksTeam();
+        TeamsEntity returnedTeam1 = teamsService.save(team1);
+
+        team1.setShortName("UPDATED SHORT NAME");
+        team1.setFullName("UPDATED FULL NAME");
+        String json = objectMapper.writeValueAsString(team1);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put("/teams/" + returnedTeam1.getShortName())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.shortName").value(returnedTeam1.getShortName())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.fullName").value("UPDATED FULL NAME")
+        );
+
+    }
+
+    @Test
+    public void testThatDeleteSuccessfullyReturnsHttp204NoContent() throws Exception{
+        TeamsEntity team1 = TestData.createKnicksTeam();
+        TeamsEntity returnedTeam1 = teamsService.save(team1);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete("/teams/" + returnedTeam1.getShortName())
+        ).andExpect(
+                MockMvcResultMatchers.status().isNoContent()
+        );
+    }
+
+    @Test
+    public void testThatDeleteSuccessfullyDeletesTeam() throws Exception{
+        TeamsEntity team1 = TestData.createKnicksTeam();
+        TeamsEntity returnedTeam1 = teamsService.save(team1);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete("/teams/" + returnedTeam1.getShortName())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.shortName").doesNotExist()
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.fullName").doesNotExist()
+        );
+    }
+
 }
