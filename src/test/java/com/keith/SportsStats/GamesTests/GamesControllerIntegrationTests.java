ae98b26992628605dc2163f09612c3f51a70f67b
Keith Cabay <keith.cabay10@gmail.com>
2023-12-04 22:39:36 -0500

    Created passing integration tests for partial update of game.

--- src/test/java/com/keith/SportsStats/GamesTests/GamesControllerIntegrationTests.java
+++ src/test/java/com/keith/SportsStats/GamesTests/GamesControllerIntegrationTests.java
@@ -18,6 +18,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 
@@ -158,6 +160,83 @@ public class GamesControllerIntegrationTests {
                 MockMvcResultMatchers.jsonPath("$.awayTeam").value(returnedAwayTeam)
         ).andExpect(
                 MockMvcResultMatchers.jsonPath("$.game_id").value(game1.getGame_id())
+        );
+    }
+
+    @Test
+    public void testThatGamePartialUpdateSuccessfullyReturnsHttp200() throws Exception{
+        GamesEntity game1 = TestData.createGameNov25();
+
+        TeamsEntity team1 = TestData.createBucksTeam();
+        TeamsEntity returnedHomeTeam = teamsService.save(team1);
+        TeamsEntity team2 = TestData.createKnicksTeam();
+        TeamsEntity returnedAwayTeam = teamsService.save(team2);
+
+        GamesEntity returnedGame =  gamesService.save(game1, returnedHomeTeam.getShortName(), returnedAwayTeam.getShortName());
+
+        game1.setHomeTeamScore(115);
+        game1.setAwayTeamScore(112);
+        game1.setDate(LocalDate.of(2023, 12, 1));
+        game1.setTime(LocalTime.of(19, 30));
+        String json = objectMapper.writeValueAsString(game1);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.patch("/games/" + returnedGame.getGame_id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        );
+
+    }
+
+    @Test
+    public void testThatGamePartialUpdateWithNonExistingIdReturns204NotFound() throws Exception{
+        GamesEntity game1 = TestData.createGameNov25();
+        String json = objectMapper.writeValueAsString(game1);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.patch("/games/" + 999)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNotFound()
+        );
+    }
+
+    @Test
+    public void testThatGamePartialUpdateReturnsPartiallyUpdatedTeam() throws Exception{
+        GamesEntity game1 = TestData.createGameNov25();
+
+        TeamsEntity team1 = TestData.createBucksTeam();
+        TeamsEntity returnedHomeTeam = teamsService.save(team1);
+        TeamsEntity team2 = TestData.createKnicksTeam();
+        TeamsEntity returnedAwayTeam = teamsService.save(team2);
+
+        GamesEntity returnedGame =  gamesService.save(game1, returnedHomeTeam.getShortName(), returnedAwayTeam.getShortName());
+
+        game1.setHomeTeamScore(115);
+        game1.setAwayTeamScore(112);
+        game1.setDate(LocalDate.of(2023, 12, 1));
+        game1.setTime(LocalTime.of(19, 30));
+
+        String json = objectMapper.writeValueAsString(game1);
+
+        //when calling toString on time it drops the seconds, so format is needed
+        String formattedTime = game1.getTime().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.patch("/games/" + returnedGame.getGame_id())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.homeTeamScore").value(game1.getHomeTeamScore())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.awayTeamScore").value(game1.getAwayTeamScore())
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.time").value(formattedTime)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.date").value(game1.getDate().toString())
         );
     }
 }
