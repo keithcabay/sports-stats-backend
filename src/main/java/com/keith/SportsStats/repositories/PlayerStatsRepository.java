520d5b95daf59cd6200584b1acc750ccc24e2b55
Keith Cabay <keith.cabay10@gmail.com>
2023-12-05 01:50:18 -0500

    Creat custom query to get list of a player's stats by game.

--- src/main/java/com/keith/SportsStats/repositories/PlayerStatsRepository.java
+++ src/main/java/com/keith/SportsStats/repositories/PlayerStatsRepository.java
@@ -2,9 +2,15 @@ package com.keith.SportsStats.repositories;
 
 import com.keith.SportsStats.domains.entity.PlayerStatsByGameEntity;
 import com.keith.SportsStats.domains.entity.PlayerStatsId;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlayerStatsRepository extends CrudRepository<PlayerStatsByGameEntity, PlayerStatsId> {
+
+    @Query("SELECT ps FROM PlayerStatsByGameEntity ps WHERE ps.player.player_id = :player_id")
+    List<PlayerStatsByGameEntity> allPlayerStats(Long player_id);
 }
