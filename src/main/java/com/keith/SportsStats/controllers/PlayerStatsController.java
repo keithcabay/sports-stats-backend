1ca9867af738230842736ef96f874a619866002b
Keith Cabay <keith.cabay10@gmail.com>
2023-12-05 00:46:45 -0500

    Create rest controller for player-stats and created post endpoint.

---
+++ src/main/java/com/keith/SportsStats/controllers/PlayerStatsController.java
@@ -0,0 +1,42 @@
+package com.keith.SportsStats.controllers;
+
+import com.keith.SportsStats.domains.dto.PlayerStatsByGameDto;
+import com.keith.SportsStats.domains.entity.PlayerStatsByGameEntity;
+import com.keith.SportsStats.mappers.impl.PlayerStatsMapper;
+import com.keith.SportsStats.services.player_stats_services.PlayerStatsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlayerStatsController {
+
+    PlayerStatsService playerStatsService;
+
+    PlayerStatsMapper playerStatsMapper;
+
+    public PlayerStatsController(PlayerStatsService playerStatsService, PlayerStatsMapper playerStatsMapper){
+        this.playerStatsService = playerStatsService;
+        this.playerStatsMapper = playerStatsMapper;
+    }
+
+    @PostMapping(path = "/player-stats/{player_id}/{game_id}")
+    public ResponseEntity<PlayerStatsByGameDto> addNewPlayerStat
+            (@PathVariable("player_id") Long player_id
+            , @PathVariable("game_id") Long game_id
+            , @RequestBody PlayerStatsByGameDto playerStatsDto){
+
+        if(!playerStatsService.playerAndGameExist(player_id, game_id)){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        PlayerStatsByGameEntity playerStatsEntity = playerStatsMapper.mapFrom(playerStatsDto);
+        PlayerStatsByGameEntity returnedPlayerStatsEntity = playerStatsService.save(playerStatsEntity, player_id, game_id);
+        PlayerStatsByGameDto returnedPlayerStatsDto = playerStatsMapper.mapTo(returnedPlayerStatsEntity);
+
+        return new ResponseEntity<>(returnedPlayerStatsDto, HttpStatus.CREATED);
+    }
+}
